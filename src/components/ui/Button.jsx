c2255159f50b4f9287825f896ba9e5104fd86493
itsSakshii <its.sakshii.me@gmail.com>
2024-10-14 19:58:37 +0530

    button check

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 export default function Button({ text }) {
   return (
     <button className="btn font-medium tracking-wide text-black capitalize 
-    transition-colors duration-300 transform bg-teal-400 rounded-xl hover:bg-teal-300 
-    focus:outline-none inline-block focus:ring focus:ring-black focus:ring-opacity-80 px-4 py-2">
-       
+      transition-colors duration-300 transform bg-teal-400 rounded-xl 
+      hover:bg-teal-300 focus:outline-none inline-block 
+      focus:ring-4 focus:ring-white focus:ring-opacity-100 
+      focus:ring-offset-2 focus:ring-offset-teal-400 
+      active:ring-white active:ring-offset-2 
+      px-4 py-2">
       {text}
     </button>
   );
